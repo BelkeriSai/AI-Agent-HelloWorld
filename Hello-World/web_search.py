c92084feb5a925a008efd2fd3df8df54e895a15d
BelkeriSai <saiprakashbelkeri@gmail.com>
2025-01-26 22:59:01 +0530

    added .env

--- Hello-World/web_search.py
+++ Hello-World/web_search.py
@@ -1,10 +1,16 @@
+import os
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openaiKey = os.getenv("OPEN_AI_KEY")
 
 web_agent = Agent(
     name = "Web Agent",
-    model = OpenAIChat(id="gpt-4o"),
+    model = OpenAIChat(id="gpt-3.5-turbo", api_key=openaiKey),
     tools = [DuckDuckGo()],
     instructions=["Always include sources"],
     show_tool_calls=True,
